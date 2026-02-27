aafbabbb9b1e9d2523b51ca639ed80863d62c82f
ThunchanokKumpha <thunchanok.kum@kkumail.com>
2026-02-27 22:54:56 +0700

    for run on localhost

--- code/backend/server.js
+++ code/backend/server.js
@@ -20,27 +20,20 @@ app.use(helmet());
 
 const allowedOrigins = [
   'http://csse2268.cpkku.com',
-  'https://csse2268.cpkku.com'
+  'https://csse2268.cpkku.com',
+  'http://localhost:3001'
 ];
 
 const corsOptions = {
-    origin: ['http://localhost:3001',
-        'https://amazing-crisp-9bcb1a.netlify.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 };
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// };
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
