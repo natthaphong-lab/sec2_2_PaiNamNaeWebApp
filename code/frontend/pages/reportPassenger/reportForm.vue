1f702931711fb67169f06f4728b7c77c2856fa8d
natthaphong-lab <natthaphong.li@kkumail.com>
2026-03-02 14:15:42 +0700

    Merge branch 'thunchanok5061' of https://github.com/natthaphong-lab/sec2_2_PaiNamNaeWebApp into Natthaphon Please enter a commit message to explain why this merge is necessary,

--- code/frontend/pages/reportPassenger/reportForm.vue
+++ code/frontend/pages/reportPassenger/reportForm.vue
@@ -2,14 +2,11 @@
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
-import SafetyForm from '@/components/Passenger_report_Driver/SafetyForm.vue'
-<<<<<<< HEAD
-=======
+
 import driverBehaviorForm from '@/components/Passenger_report_Driver/driverBehaviorForm.vue'
 import vehicleForm from '@/components/Passenger_report_Driver/vehicleForm.vue'
 import lostFoundForm from '@/components/Passenger_report_Driver/lostFoundForm.vue'
 import otherForm from '@/components/Passenger_report_Driver/otherForm.vue'
->>>>>>> bcb04291abeb041c6acce2435a40c77854bcbbbe
 
 const route = useRoute()
 
