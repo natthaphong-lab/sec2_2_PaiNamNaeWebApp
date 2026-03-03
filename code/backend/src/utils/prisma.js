3d6df99f628926fbd7b0580c5438749fb4debe3c
iinuXD <kittiphat.ra@kkumail.com>
2026-03-03 16:11:23 +0700

    prisma.js

--- code/backend/src/utils/prisma.js
+++ code/backend/src/utils/prisma.js
@@ -1,12 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    prisma = new PrismaClient({ adapter });
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient();
+        const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+        const adapter = new PrismaPg(pool);
+        global.prisma = new PrismaClient({ adapter });
     }
     prisma = global.prisma;
 }
