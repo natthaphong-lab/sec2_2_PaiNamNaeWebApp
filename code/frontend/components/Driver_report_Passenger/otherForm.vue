633ce939455e500952f7c9e2acee16cc859d315a
iinuXD <kittiphat.ra@kkumail.com>
2026-03-03 14:35:37 +0700

    fix use correct id

--- code/frontend/components/Driver_report_Passenger/otherForm.vue
+++ code/frontend/components/Driver_report_Passenger/otherForm.vue
@@ -112,7 +112,7 @@ async function submitForm() {
 
 let finalTypes = [selectedIssue.value]
 
-  formData.append('reportedUserId', driverId)
+  formData.append('reportedUserId', passengerId)
   formData.append('category', category)
   formData.append('types[]', 'ปัญหาอื่น ๆ')
   formData.append('description', description.value)
