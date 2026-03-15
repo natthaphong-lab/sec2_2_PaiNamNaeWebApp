048253f3478ec960b9648102d761484e96e34478
ThunchanokKumpha <thunchanok.kum@kkumail.com>
2026-03-15 22:50:37 +0700

    Enhance notification color

--- code/frontend/composables/useToast.js
+++ code/frontend/composables/useToast.js
@@ -2,6 +2,13 @@ import { ref } from 'vue'
 
 const toasts = ref([])
 
+const statusToToastType = {
+  PENDING: 'warning',
+  ON_PROGRESS: 'info',
+  COMPLETED: 'success',
+  REJECTED: 'error'
+}
+
 export function useToast() {
     const addToast = (toast) => {
         toasts.value.push({ ...toast, id: Date.now() })
@@ -12,6 +19,7 @@ export function useToast() {
     }
 
     const toast = {
+        
         success: (title, message, duration) => {
             addToast({ type: 'success', title, message, duration })
         },
@@ -24,6 +32,12 @@ export function useToast() {
         info: (title, message, duration) => {
             addToast({ type: 'info', title, message, duration })
         },
+
+        // ⭐ ต้องอยู่ใน object นี้
+        status: (status, title, message, duration) => {
+            const type = statusToToastType[status] || 'info'
+            addToast({ type, title, message, duration })
+        }
     }
 
     return { toasts, addToast, removeToast, toast }
